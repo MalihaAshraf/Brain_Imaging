b30f27dc2c8774754cb393d4ffc10006d1e6d54f
Maliha Ashraf <malihaa266@gmail.com>
2020-05-17 16:19:51 +0100

    t-test analysis

--- plot_per_region.m
+++ plot_per_region.m
@@ -11,17 +11,17 @@ colors = lines(n_sub);
 data_folder = 'datasets/DTI/';
 
 mean_all_subj = false;   % Mean of all subjects per region
-ind_sub_plot = false;   % subplot for individual subjects
 norm = true;           % Normalize data, true or false
 fit_ = true;           % Fit data, true or false
 log_ = true;            % No. of days in log, true or false
 e_bar = false;           % Error bar, true or false
 
 rep = true;             % Generate pdf report true or false
-rep_name = 'figs/DTI_analysis_may_4th_20';
+rep_name = 'figs/DTI_analysis_may_10th_20';
 
 doc_ = true;
 doc_name = 'tables/DTI_stats.xlsx';
+doc2_name = 'tables/DTI_stats_all.xlsx';
 
 if rep
     import mlreportgen.report.*
@@ -58,18 +58,32 @@ table_vars = [["Regions", "int16"];...
     ["p-value", "double"];...
     ["intercept", "double"];...
     ["slope", "double"]];
-
+table2_vars = [["Regions", "int16"];...
+    ["Diff_type", "string"];...
+    ["T-test", "double"];...
+    ["ci_low", "double"];...
+    ["ci_high", "double"];...
+    ["p-value-test", "double"];...
+    ["RMSE", "double"];...
+    ["R_squared", "double"];...
+    ["Adj_R_squared", "double"];...
+    ["F-statistic", "double"];...
+    ["p-value-fit", "double"];...
+    ["intercept", "double"];...
+    ["slope", "double"]];
 T = table('Size',[numel(regions)*numel(types)*n_sub,size(table_vars,1)],... 
 	'VariableNames', table_vars(:,1),...
 	'VariableTypes', table_vars(:,2));
+T_all = table('Size',[numel(regions)*numel(types),size(table2_vars,1)],... 
+	'VariableNames', table2_vars(:,1),...
+	'VariableTypes', table2_vars(:,2));
 j = 1;
+k = 1;
 
 for rr = 1:numel(regions)
 
     for tt = 1:numel(types)
         clear ds
-%         subplot(1, n_sub, ss+(tt-1)*n_sub)
-%         subplot(1, 4, tt)
 
         type = types{tt};
          
@@ -82,11 +96,10 @@ for rr = 1:numel(regions)
             figure
         end
         
-        if mean_all_subj
-           days_all = [];
-           data_all = []; 
-        end
+       days_all = [];
+	   data_all = []; 
         
+	   slopes = [];
         for ss = 1:n_sub
             if (~mean_all_subj) && (ind_sub_plot)
                 subplot(3, 5, ss);
@@ -122,7 +135,6 @@ for rr = 1:numel(regions)
                 data_sd = data_sd./norm_param;
             end
             
-            if ~mean_all_subj
                 if e_bar
                     stdshade(cat(2, data, data_sd),0.2, colors(ss, :), days_v);
                 else
@@ -137,17 +149,13 @@ for rr = 1:numel(regions)
                     T(j, :) = {rr, type, ss, mdl.NumObservations,...
                         mdl.RMSE, mdl.Rsquared.Ordinary, mdl.Rsquared.Adjusted,...
                         F, p, mdl.Coefficients.Estimate(1), mdl.Coefficients.Estimate(2)};
-                    
-    %                    f = fit(days_v, data, 'gauss2', 'Weights', data_sd);
-%                        y = feval(f, days_v(1):0.001:days_v(end));
-%                        y_p = y+d*(ss-1);
+                    slopes = [slopes;  mdl.Coefficients.Estimate(2)];
+
                        plot(days_v, mdl.Fitted, 'Color', colors(ss, :), 'LineStyle', '--');
                 else
                        hh(ss) = plot(days_v, data', 'Color', colors(ss, :));
                 end
-%                 hold on
 
-    %             label{ss} = ['Subject ' num2str(ss)];
                 i = i+ n_v;
                 j = j+1;
                 
@@ -160,9 +168,6 @@ for rr = 1:numel(regions)
                     grid on
                 end
                 
-               
-            else
-                hh(ss) = scatter(days_v, data', 5, colors(ss, :), 'filled');  
                 hold on
                 if ss == 1
                    days_all = days_v;
@@ -174,10 +179,10 @@ for rr = 1:numel(regions)
                     data_all = data_all(ind);
                     clear ind
                 end
-            end  
+              
         end
         
-        if mean_all_subj
+%         if mean_all_subj
             [days_all_u,ia,ic] = unique(days_all);
             data_all_u = ones(size(days_all_u)).*NaN;
             data_sd_u = ones(size(days_all_u)).*NaN;
@@ -189,54 +194,28 @@ for rr = 1:numel(regions)
                 n_u(uu) = length(find(ic == uu));
             end
             
-            if 1 % choose between error region or error bar
-                stdshade(cat(2, data_all_u, data_sd_u),0.2, [0.5, 0.5, 0.5], days_all_u);
-%                 hold on
-%                 scatter(days_all, data_all, 3, 'filled');
-            else
-                errorbar(days_all_u, data_all_u, data_sd_u);
-            end
-            
+
             if fit_
-                ft = '(p*(a1*exp(-((x-b1)/c1)^2)+d1)) + ((1-p)*(a2*exp(-((x-b2)/c2)^2)+d2))';
-                [~, ind] = max(abs(data_all_u - mean(data_all_u)));
-                if data_all_u(ind) < 1
-                    st_pts = [-0.5, -0.5, 1.1, 2.1, 0.1, 0.5, 0.5, 1, 0.5];
-                else
-                    st_pts = [0.5, 0.5, 1.1, 2.1, 0.1, 0.5, 0.5, 1, 0.5];
-                end
+                mdl = fitlm(days_all_u, data_all_u, 'Weights', data_sd_u);
+                [p,F] = coefTest(mdl);
+                [h1,p2,ci,stats] = ttest(slopes);
+                T_all(k, :) = {rr, type,  h1, ci(1), ci(2), p2,...
+                    mdl.RMSE, mdl.Rsquared.Ordinary, mdl.Rsquared.Adjusted,...
+                    F, p, mdl.Coefficients.Estimate(1), mdl.Coefficients.Estimate(2)};
+                k = k+1;
 
-                f = fit(days_all_u, data_all_u, ft,...
-                    'StartPoint', st_pts);
-
-%                    f = fit(days_v, data, 'gauss2', 'Weights', data_sd);
-                   y = feval(f, days_all_u(1):0.001:days_all_u(end));
-                   y_p = y+d*(ss-1);
-                   hh(ss) = plot(days_all_u(1):0.001:days_all_u(end), y',... 
-                       'LineStyle', '--', 'Color', [0.5, 0.5, 0.5]);
+               plot(days_all_u, mdl.Fitted,... 
+                       'LineWidth', 2, 'LineStyle', '-', 'Color', [0 0 0]);
 
              end
             
-%             xticks = [-5 0 10 30 60 100 200];
             xticks_l = (linspace(days_all_u(1), days_all_u(end), 10));
-            xticks = round(10.^(xticks_l)-10);
+            xticks = round(10.^(xticks_l)-d_l);
             set(gca, 'XTick', xticks_l);
             set(gca', 'XTickLabel', num2str(xticks'));
             xlabel('Scanning days')
-            ylabel('Median z-score normalized data (error region: iqr)')
+            ylabel('Mean diffusivity')
             grid on
-        else
-             if ~ind_sub_plot
-                    xlim([days_min days_max]);
-                    xticks_l = (linspace(0, days_max, 10));
-                    xticks = round(10.^(xticks_l)-d_l);
-                    set(gca, 'XTick', xticks_l);
-                    set(gca', 'XTickLabel', num2str(xticks'));
-                    xlabel('Scanning days')
-                    ylabel('Mean diffusivity')
-                    grid on
-             end
-        end
         
         suptitle([type, ' Region ', num2str(regions(rr))]);
     
@@ -249,10 +228,7 @@ for rr = 1:numel(regions)
         end
     
     end
-    
-%     subplot(1, numel(types)+1, numel(types)+1)
-%     suptitle(['Region ', num2str(regions(rr))]);
-    
+        
 end
 
 if rep
@@ -261,4 +237,5 @@ end
 
 if doc_
    writetable(T,doc_name) 
+   writetable(T_all, doc2_name)
 end
--- plot_per_region_wm_ASEG.m
+++ plot_per_region_wm_ASEG.m
@@ -8,125 +8,164 @@ load days2.mat                       % Day number for each visit for each partic
 % regions = [ 4 5 6 23 24 25 26 27 28 29 42 43 44 45];
 regions = 1:191;
 colors = lines(n_sub);
+data_folder = 'datasets/ASEG/';
 labels = readtable('ASEG_label_list.xlsx');
 
-mean_per_subj = true;   % Mean of all subjects per region
+mean_per_subj = false;   % Mean of all subjects per region
 norm = true;           % Normalize data, true or false
 fit_ = true;           % Fit data, true or false
 log_ = true;            % No. of days in log, true or false
-e_bar = true;           % Error bar, true or false
+e_bar = false;           % Error bar, true or false
+ind_sub_plot = false;
 
 rep = true;             % Generate pdf report true or false
-rep_name = 'Per_subject_znorm_median_aseg';
+rep_name = 'figs/ASEG_FAMask_may_16th_20'; 
+
+doc_ = true;
+doc_name = 'tables/ASEG_FAMask_stats.xlsx';
+doc2_name = 'tables/ASEG_FAMask_stats_all.xlsx';
 
 if rep
     import mlreportgen.report.*
     import mlreportgen.dom.*
     rpt = Report(rep_name,'pdf');
-    chap = Chapter('Median z-score normalized data');
+    chap = Chapter('Linear fitting');
     add(rpt,chap);
     rpt.Document.CurrentPageLayout.PageMargins.Left = '0.5in';
     rpt.Document.CurrentPageLayout.PageMargins.Right = '0.5in';
 end
 
 if rep
-    figure
+    h=figure('visible','off');
+    h.PaperUnits = 'inches';
+    h.PaperSize = [7.5 4.5];
 %     figure('units','normalized','outerposition',[0.1 0 0.9 1])
 end
 
-for rr = 1:numel(regions)
+table_vars = [["Regions", "int16"];...
+    ["Diff_type", "string"];...
+    ["Subject", "int16"];...
+    ["n", "int16"];...
+    ["RMSE", "double"];...
+    ["R_squared", "double"];...
+    ["Adj_R_squared", "double"];...
+    ["F-statistic", "double"];...
+    ["p-value", "double"];...
+    ["intercept", "double"];...
+    ["slope", "double"]];
+table2_vars = [["Regions", "int16"];...
+	["Region_name", "string"];...
+    ["Diff_type", "string"];...
+    ["T-test", "double"];...
+    ["ci_low", "double"];...
+    ["ci_high", "double"];...
+    ["p-value-test", "double"];...
+    ["RMSE", "double"];...
+    ["R_squared", "double"];...
+    ["Adj_R_squared", "double"];...
+    ["F-statistic", "double"];...
+    ["p-value-fit", "double"];...
+    ["intercept", "double"];...
+    ["slope", "double"]];
 
+clear T T_all
+T = table('Size',[numel(regions)*numel(types)*n_sub,size(table_vars,1)],... 
+	'VariableNames', table_vars(:,1),...
+	'VariableTypes', table_vars(:,2));
+T_all = table('Size',[numel(regions)*numel(types),size(table2_vars,1)],... 
+	'VariableNames', table2_vars(:,1),...
+	'VariableTypes', table2_vars(:,2));
+j = 1;
+k = 1;
+
+for rr = 1:numel(regions)
+	r_label =  labels.labelName{rr};
     for tt = 1:numel(types)
         clear ds
-%         subplot(1, n_sub, ss+(tt-1)*n_sub)
-%         subplot(1, 4, tt)
 
         type = types{tt};
          
-         i = 1;
         if rep
-            clf
+            clf(h)
         else
 %             figure('units','normalized','outerposition',[0 0 1 1])
             figure
         end
         
-        if mean_per_subj
-           days_all = [];
-           data_all = []; 
-        end
+        days_all = [];
+		data_all = []; 
+        
+		slopes = [];
         
         for ss = 1:n_sub
-            if ~mean_per_subj
+            if (~mean_per_subj) && (ind_sub_plot)
                 subplot(3, 5, ss);
             end
             
-            n_v = n_visits(ss);
             days_v = days(:, ss);
             days_v = days_v(~isnan(days_v));
-            sel = (days_v >= 0);% & (days_v < 120);
+%            sel = (days_v >= 0);% & (days_v < 120);
 %             days_v = days_v(3:end, :);
-            days_v = days_v(sel, :);
-            d_l = 1;    % adjustment factor for log values
+            days_v(days_v < -1) = -1;
+            d_l = 2;    % adjustment factor for log values
             if log_
-                days_v = log10(days_v+1);
+                days_v = log10(days_v+d_l);
             end
-              
-            load (['ASEG/ASEG/', types{tt}, '/S', num2str(ss, '%d'), '_', types{tt}, '.mat']);
+            days_max = log10(200+d_l);
+            days_min = log10(-2+d_l);
+			
+            load ([data_folder, 'ASEG/', types{tt}, '/S', num2str(ss, '%d'), '_', types{tt}, '.mat']);
             
-            if 0%tt == 2
-                data = meanFADifValue(rr, sel)';
+            if 1%tt == 2
+                data = meanFADifValue(rr, 1:length(days_v))';
+                if ~sum(data)
+                    continue
+                end
             else
-                data = meanDifValue(rr, sel)';
+                data = meanDifValue(rr, 1:length(days_v))';
+                if ~sum(data)
+                    continue
+                end
 %                 data_sd = stdDifValue(rr, :);
             end
            
+			norm_param = mean(data);
             d = 0.2;
             if norm                
-                data = zscore(data);
-                data = data - data(1);
+                data = (data - norm_param)./norm_param; 
                 data_p = data+d*(ss-1);
             end
             
-            if ~mean_per_subj
+%             if ~mean_per_subj
                 if e_bar
                     stdshade(cat(2, data, data_sd),0.2, colors(ss, :), days_v);
     %                 errorbar(days_v, data_p', data_sd, 'Vertical', '.',...
     %                     'MarkerSize',3, 'Color',colors(ss, :), 'MarkerFaceColor',colors(ss, :))
                 else
-                    scatter(days_v, data_p', 5, colors(ss, :), 'filled');    
+                    scatter(days_v, data', 5, colors(ss, :), 'filled');    
                 end
                 hold on
                 
                 if fit_
-    %                 ft = '(p*a1*exp(-((x-b1)/c1)^2)+d1) + ((1-p)*a2*exp(-((x-b2)/c2)^2)+d2)';
-                    ft = '(p*(a1*exp(-((x-b1)/c1)^2)+d1)) + ((1-p)*(a2*exp(-((x-b2)/c2)^2)+d2))';
-                    [~, ind] = max(abs(data - mean(data)));
-                    if data(ind) < 1
-                        st_pts = [-0.5, -0.5, 1.1, 2.1, 0.1, 0.5, 0.5, 1, 0.5];
-                    else
-                        st_pts = [0.5, 0.5, 1.1, 2.1, 0.1, 0.5, 0.5, 1, 0.5];
-                    end
-    %                 f = fit(days_v, data, ft);
+					mdl = fitlm(days_v, data);
+                    [p,F] = coefTest(mdl);
+                    T(j, :) = {rr, type, ss, mdl.NumObservations,...
+                        mdl.RMSE, mdl.Rsquared.Ordinary, mdl.Rsquared.Adjusted,...
+                        F, p, mdl.Coefficients.Estimate(1), mdl.Coefficients.Estimate(2)};
+                    slopes = [slopes;  mdl.Coefficients.Estimate(2)];
 
-                    f = fit(days_v, data, ft,...
-                        'StartPoint', st_pts);
-
-    %                    f = fit(days_v, data, 'gauss2', 'Weights', data_sd);
-                       y = feval(f, days_v(1):0.001:days_v(end));
-                       y_p = y+d*(ss-1);
-                       hh(ss) = plot(days_v(1):0.001:days_v(end), y', 'Color', colors(ss, :));
+                    plot(days_v, mdl.Fitted, 'Color', colors(ss, :), 'LineStyle', '--');
                 else
                        hh(ss) = plot(days_v, data', 'Color', colors(ss, :));
                 end
                 hold on
+				j = j+1;
 
     %             label{ss} = ['Subject ' num2str(ss)];
-                i = i+ n_v;
-                title(['S' num2str(ss)])
+    %            title(['S' num2str(ss)])
 
-            else
-                hh(ss) = scatter(days_v, data', 5, colors(ss, :), 'filled');  
+%             else
+            %    hh(ss) = scatter(days_v, data', 5, colors(ss, :), 'filled');  
                 hold on
                 if ss == 1
                    days_all = days_v;
@@ -138,10 +177,10 @@ for rr = 1:numel(regions)
                     data_all = data_all(ind);
                     clear ind
                 end
-            end  
+%             end  
         end
         
-        if mean_per_subj
+%         if mean_per_subj
             [days_all_u,ia,ic] = unique(days_all);
             data_all_u = ones(size(days_all_u)).*NaN;
             data_sd_u = ones(size(days_all_u)).*NaN;
@@ -153,37 +192,22 @@ for rr = 1:numel(regions)
                 n_u(uu) = length(find(ic == uu));
             end
             
-            if 1 % choose between error region or error bar
-                stdshade(cat(2, data_all_u, data_sd_u),0.2, [0.5, 0.5, 0.5], days_all_u);
-%                 hold on
-%                 scatter(days_all, data_all, 3, 'filled');
-            else
-                errorbar(days_all_u, data_all_u, data_sd_u);
-            end
-            
             if fit_
-%                 ft = '(p*(a1*exp(-((x-b1)/c1)^2)+d1)) + ((1-p)*(a2*exp(-((x-b2)/c2)^2)+d2))';
-%                 ft = @(a, b, c1, c2, x) bigauss(x, a, b, c1, c2);
-%                 [~, ind] = max(abs(data_all_u - mean(data_all_u)));
-%                 if data_all_u(ind) < 1
-%                     st_pts = [-0.7, 1, 0.2, 0.02];
-%                 else
-%                     st_pts = [0.7, 1, 0.2, 0.02];
-%                 end
+				mdl = fitlm(days_all_u, data_all_u);
+                [p,F] = coefTest(mdl);
+                [h1,p2,ci,stats] = ttest(slopes);
+                T_all(k, :) = {rr, r_label, type,  h1, ci(1), ci(2), p2,...
+                    mdl.RMSE, mdl.Rsquared.Ordinary, mdl.Rsquared.Adjusted,...
+                    F, p, mdl.Coefficients.Estimate(1), mdl.Coefficients.Estimate(2)};
+                k = k+1;
 
-%                 f = fit(days_all_u, data_all_u, ft,...
-%                     'StartPoint', st_pts);
-                [f, gof] = fit(days_all_u(days_all_u > 0), data_all_u(days_all_u > 0), 'poly1');
-                
-%                    [f, gof] = fit(days_all_u, data_all_u, 'poly1', 'Weights', data_sd_u);
-                   y = feval(f, days_all_u(1):0.001:days_all_u(end));
-                   y_p = y+d*(ss-1);
-                   hh(ss) = plot(days_all_u(1):0.001:days_all_u(end), y',... 
-                       'LineStyle', '--', 'Color', [0.5, 0.5, 0.5]);
-                   text(days_all_u(end-15), max(data_all)*1.2,... 
-                       ['rmse: ', num2str(gof.rmse)]);
+               plot(days_all_u, mdl.Fitted,... 
+                       'LineWidth', 2, 'LineStyle', '-', 'Color', [0 0 0]);
+					   
+                %text(days_all_u(end-15), max(data_all)*1.2,... 
+                 %      ['rmse: ', num2str(gof.rmse)]);
 
-             end
+			end
                         
              
 %             xticks = [-5 0 10 30 60 100 200];
@@ -192,11 +216,11 @@ for rr = 1:numel(regions)
             set(gca, 'XTick', xticks_l);
             set(gca', 'XTickLabel', num2str(xticks'));
             xlabel('Scanning days')
-            ylabel('Median z-score normalized data (error region: iqr)')
+            ylabel('Mean diffusivity')
             grid on
-        end
+%         end
         
-        suptitle([type, ' ', labels.labelName{rr}]);
+        suptitle([type, ' Region ', num2str(rr), ' ', labels.labelName{rr}]);
          
 %         ylim([0.8 d*20])
 
@@ -206,11 +230,11 @@ for rr = 1:numel(regions)
 %         end
     
         if rep
-            fig = Figure(gcf);
-            fig.Width = '8in';
-            fig.Height = '5in';
-            fig.Scaling = 'custom';
-            add(rpt, fig);
+%            fig = Figure(gcf);
+%            fig.Width = '8in';
+%            fig.Height = '5in';
+%            fig.Scaling = 'custom';
+            add(rpt, Figure(h));
         end
     
     end
@@ -223,3 +247,11 @@ end
 if rep
     close(rpt);
 end
+
+if doc_
+   writetable(T,doc_name) 
+   writetable(T_all, doc2_name)
+end
+
+close all
+h=figure('visible','on');
